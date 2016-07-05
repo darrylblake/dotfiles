993d22e4d1804a8ed6fa5d8f518b808e240d83fa
Darryl Blake <darryl@darrylblake.com>
2016-07-06 11:54:54 +1200

    Adding sublime preferences.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Creates symlinks from the home directory to dotfiles in ~/dotfiles
+# # Creates symlinks from the home directory to dotfiles in ~/dotfiles
 
 dir=~/dotfiles
 backup=~/dotfiles_backup
-files=".gitconfig .zshrc" 
+files=".gitconfig .zshrc"
 
 # Create backup folder
 mkdir -p $backup
@@ -14,10 +14,22 @@ cd $dir
 # Delete old backup
 rm -ri $backup
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
   echo "Moving any previous dotfiles to $backup"
   mv ~/$file $backup
-  echo "Creating symlinks to $file in home directory."
+  echo "Creating dotfile symlinks..."
   ln -s $dir/$file ~/$file
+done
+
+# Sublime
+dirSublimePackages=/Users/darryl/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+sublimeFiles="Preferences.sublime-settings"
+
+for file in $sublimeFiles; do
+  echo "Moving any previous files to $backup"
+  mv "$dirSublimePackages/$file" $backup
+  echo "Creating Sublime Text symlinks..."
+  ln -s $dir/$file "$dirSublimePackages/$file"
 done
