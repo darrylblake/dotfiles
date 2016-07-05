cb19daf966d5d5bea185fe0b53aac3c4c2525b94
Darryl Blake <darryl@darrylblake.com>
2016-07-05 18:00:04 +1200

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+dir=~/dotfiles
+backup=~/dotfiles_backup
+files=".gitconfig .zshrc" 
+
+# Create backup folder
+mkdir -p $backup
+
+# Change to the dotfiles directory
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+  echo "Moving any previous dotfiles to $backup"
+  mv ~/$file $backup
+  echo "Creating symlinks to $file in home directory."
+  ln -s $dir/$file ~/$file
+done
